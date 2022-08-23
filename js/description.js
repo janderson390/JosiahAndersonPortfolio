5b35b20241f0f12ed069c227ded7a2cb4244bc64
janderson390 <janderson390@student.cvtc.edu>
2022-08-23 12:31:40 -0500

    Initial Commit

---
+++ js/description.js
@@ -0,0 +1,9 @@
+let description = "I am a recent college graduate from Chippewa Valley Technical College's IT - Software Developer program. This has given me an Associate of Applied Science in Software Development, which I am leveraging to find a career as a Junior Software Developer. I am unsure of the career path I would like to take in Software Development, but I do have the most expirience in front-end development.";
+
+document.write(
+  `
+  <p class=\"about\">
+    ` + description + `
+  <p>
+  `
+);
